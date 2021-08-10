aeae65f4e0014d3d0e03b609b8ded0780cc721ec
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-08-10 18:54:38 +0200

    added user authentication token

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,15 @@
+class V1::SessionsController << ApplicationController
+  def create
+    member = Member.where(email: params[:email]).first
+
+    if member&.valid_password?(params[:password])
+      render json: user.as_json(only: [:email, :authentication_token]), status: :created
+    else
+      head(:unauthorized)
+    end
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  # devise_for :members
   namespace :api do
       resources :users
+      resources :sessions, only: [:create, :destroy]
   end
-  
-  
-  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
